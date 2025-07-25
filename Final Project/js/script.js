870d09e8066ab44f36ecb571073f0ace04822752
Abdelrahman7cod <abdelrahmanamgad7@gmail.com>
2025-07-25 22:34:10 +0300

    Final Project

---
+++ Final Project/js/script.js
@@ -0,0 +1,14 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Form submitted!'); 
+});
+
+
+function initMap() {
+  const office = { lat: 18.5355, lng: 73.8851 }; 
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: office
+  });
+  new google.maps.Marker({ position: office, map });
+}
